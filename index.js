2216dd5e62f7f36ee17443fcf4e12abaf6b2a911
Bevan Rudge <bevan@geneious.com>
2019-05-10 17:19:24 +1200

    BX-4126 Fail more drastically in Angular, so that it doesn't ignore

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ module.exports = {
             if (config.singleRun && browser.lastResult.skipped) {
                 const message = '⁉️ Tests skipped! Are you still using fit(), xit(), fdescribe() or xdescribe()?'
                 console.error(message)
-                throw new Error(message)
+
+                // Something about `ng test` (Angular's version of Karma) causes it to ignore failure from
+                // `throw new Error()` and exit with successful status code of 0. Crash a bit more severely
+                // if the Karma configuration looks like it is for Angular. This causes stand-alone Karma
+                // to leave the browser running. So just `throw new Error()` for other scenarios.
+                if (config.reporters.includes('@angular-devkit/build-angular--event-reporter')) {
+                    process.exit(1)
+                } else {
+                    throw new Error(message)
+                }
             }
         }
     }],
